f4132e3e9330e6c1db4f82af62d515f1b041f086
Raphael Hetzel <raphael.hetzel@gmail.com>
2020-03-05 17:22:07 +0100

    Fix waiting for time, boards.yml

--- main/src_esp32/main.cpp
+++ main/src_esp32/main.cpp
@@ -202,6 +202,7 @@ void main_task(void*) {
   size_t retries = 0;
   while (t < 1577836800 && retries < 60) {
     printf("waiting for time\n");
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
     retries++;
   }
 
