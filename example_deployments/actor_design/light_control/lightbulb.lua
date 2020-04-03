5bdf1a15ea170d9874927f2a093b81cb986773af
Raphael Hetzel <raphael.hetzel@gmail.com>
2020-04-03 23:10:34 +0200

    Fix boards file, adapt light control application from the thesis.

--- example_deployments/actor_design/light_control/lightbulb.lua
+++ example_deployments/actor_design/light_control/lightbulb.lua
@@ -1,30 +1,28 @@
 function receive(message)
-  
   if(message.type == "init") then
     publish({node_id=node_id, type="label_get", key="core.location.room"})
-    -- Light is initialized to off
-    publish({node_id=node_id, actor_type="core.io.gpio", command="gpio_output_set_level", gpio_pin=27, gpio_level=1})
-    light_on = false
-  end
-  
-  if(message.type == "label_response" and message.key == "core.location.room") then
+    deferred_block_for({node=node_id, type="label_response"}, 2000)
+    light_off() -- Light is initialized to off
+  elseif(message.type == "label_response"
+          and message.key == "core.location.room") then
     sub_id = subscribe({type="light_control", room=message.value})
-  end
-
-  if(message.type == "exit") then
-    if(light_on) then
-      light_on = false
-      publish({node_id=node_id, actor_type="core.io.gpio", command="gpio_output_set_level", gpio_pin=27, gpio_level=1})   
+  elseif(message.type == "exit" and light_status_on) then
+      light_off()
+  elseif(message.type == "light_control") then
+    if(message.command == "off" or (message.command == "switch" and light_status_on)) then
+      light_off()
+    elseif(message.command == "on" or (message.command == "switch" and not light_status_on)) then
+      light_on()
     end
+  elseif(message.type == "timeout") then
+    print("received timeout instead of room information")
   end
-  
-  if(message.type == "light_control") then
-    if(message.command == "off" or (message.command == "switch" and light_on)) then
-      light_on = false
-      publish({node_id=node_id, actor_type="core.io.gpio", command="gpio_output_set_level", gpio_pin=27, gpio_level=1})
-    elseif(message.command == "on" or (message.command == "switch" and not light_on)) then
-      light_on = true
-      publish({node_id=node_id, actor_type="core.io.gpio", command="gpio_output_set_level", gpio_pin=27, gpio_level=0})
-    end
-  end
+end
+function light_on()
+  light_status_on = true
+  publish({node_id=node_id, actor_type="core.io.gpio", command="gpio_output_set_level", gpio_pin=27, gpio_level=0})
+end
+function light_off()
+  light_status_on = false
+  publish({node_id=node_id, actor_type="core.io.gpio", command="gpio_output_set_level", gpio_pin=27, gpio_level=1})
 end
--- example_deployments/actor_design/light_control/lightswitch.lua
+++ example_deployments/actor_design/light_control/lightswitch.lua
@@ -1,26 +1,16 @@
 function receive(message)
-  
   if(message.type == "init") then
+    last_trigger = 0
     publish({node_id=node_id, type="label_get", key="core.location.room"})
-    deferred_block_for({node=node_id, type=label_get}, 2000)
-  end
-
-  if(message.type == "timeout") then
-    print("received timeout instead of room information")
-  end
-  
-  if(message.type == "label_response" and message.key == "core.location.room") then
+    deferred_block_for({node=node_id, type="label_response"}, 2000)
+  elseif(message.type == "label_response" 
+         and message.key == "core.location.room") then
     room_id = message.value
     subscribe({gpio_pin=33, publisher_node_id=node_id, type="gpio_update"})
-  end
-  
-  if(message.type == "exit") then
-    print("lightswitch exit")
-  end
-
-  if(message.type == "gpio_update" and message.gpio_level == 0) then
-    print("receive trigger")
+  elseif(message.type == "gpio_update" and message.gpio_level == 0 and now() - last_trigger > 250) then
     publish({type="light_control", command="switch", room=room_id})
+    last_trigger = now()
+  elseif(message.type == "timeout") then
+    print("received timeout instead of room information")
   end
-
 end
