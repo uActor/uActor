4e96da027829111c0284dc03cf1bb5705b8a9478
Raphael Hetzel <hetzel@in.tum.de>
2020-08-20 11:18:45 +0200

    Remove legacy code (test actors, hierarchical routing)

--- main/src_esp32/main.cpp
+++ main/src_esp32/main.cpp
@@ -37,85 +37,7 @@ extern "C" {
 void app_main(void);
 }
 
-#if CONFIG_LEGACY_BENCHMARK_ENABLED
-
-#include "../legacy/benchmark_actor.hpp"
-#include "../legacy/benchmark_configuration.hpp"
-#include "../legacy/benchmark_lua_actor.hpp"
-#include "../legacy/include/message.hpp"
-#include "../legacy/include/router_v2.hpp"
-// #include "../legacy/router.hpp"
-
-struct BenchmarkParams {
-  uint64_t id;
-  RouterV2* router;
-};
-
-void actor_task(void* params) {
-  RouterV2* router = ((struct BenchmarkParams*)params)->router;
-  uint64_t id = ((struct BenchmarkParams*)params)->id;
-
-#if LUA_ACTORS
-#if LUA_SHARED_RUNTIME
-  lua_State* lua_state = LuaActor::create_state();
-#else
-  lua_State* lua_state = nullptr;
-#endif
-  LuaActor* actor[ACTORS_PER_THREAD];
-  for (int i = 0; i < ACTORS_PER_THREAD; i++) {
-    actor[i] = new LuaActor(id * ACTORS_PER_THREAD + i, lua_state);
-  }
-#else
-  Actor* actor[ACTORS_PER_THREAD];
-  for (int i = 0; i < ACTORS_PER_THREAD; i++) {
-    actor[i] = new Actor(id * ACTORS_PER_THREAD + i);
-  }
-#endif
-  char primary[128], buffer[128];
-  snprintf(primary, sizeof(primary), "%lld", id * ACTORS_PER_THREAD);
-  router->register_actor(primary);
-  for (int i = 1; i < ACTORS_PER_THREAD; i++) {
-    snprintf(buffer, sizeof(buffer), "%lld", id * ACTORS_PER_THREAD + i);
-    router->register_alias(primary, buffer);
-  }
-
-  while (true) {
-    auto message = router->receive(primary, portMAX_DELAY);
-    if (message) {
-      actor[std::stoi(message->receiver()) - id * ACTORS_PER_THREAD]->receive(
-          std::stoi(message->sender()), const_cast<char*>(message->buffer()));
-    }
-  }
-}
-
-void real_main(void*) {
-  printf("InitialHeap: %d \n", xPortGetFreeHeapSize());
-  RouterV2* router = &RouterV2::getInstance();
-
-  // TaskHandle_t handles[NUM_THREADS] = {NULL};
-  BenchmarkParams* params = static_cast<BenchmarkParams*>(
-      pvPortMalloc(sizeof(BenchmarkParams) * NUM_THREADS));
-
-  for (uint64_t i = 0; i < NUM_THREADS; i++) {
-    params[i] = {.id = i, .router = router};
-    char buffer[16];
-    snprintf(buffer, sizeof(buffer), "Test%lld", i);
-    xTaskCreatePinnedToCore(&actor_task, buffer, 7500, &params[i], 5, nullptr,
-                            i % 2);
-  }
-
-  vTaskDelay(1000 / portTICK_PERIOD_MS);
-  printf("StaticHeap: %d \n", xPortGetFreeHeapSize());
-  router->send(Message("0", "1", 1234, ""));
-  vTaskDelete(nullptr);
-}
-
-void app_main(void) {
-  xTaskCreatePinnedToCore(&real_main, "MAIN", 7500, nullptr, 5, nullptr, 0);
-}
-#else
-
-void main_task(void*) {
+void main_task(void *) {
   printf("InitialHeap: %d \n", xPortGetFreeHeapSize());
 
   xTaskCreatePinnedToCore(&uActor::PubSub::Router::os_task, "Router", 4192,
@@ -315,5 +237,3 @@ void app_main(void) {
 
   xTaskCreatePinnedToCore(&main_task, "MAIN", 4192, nullptr, 5, nullptr, 0);
 }
-
-#endif
