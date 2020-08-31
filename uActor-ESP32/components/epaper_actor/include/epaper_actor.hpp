acf5052643e36f28dd89ba367c0c7171fc6821af
Raphael Hetzel <hetzel@in.tum.de>
2020-08-31 10:30:08 +0200

    Add  components to the lint target

--- uActor-ESP32/components/epaper_actor/include/epaper_actor.hpp
+++ uActor-ESP32/components/epaper_actor/include/epaper_actor.hpp
@@ -1,85 +1,80 @@
 #ifndef UACTOR_ESP32_COMPONENTS_EPAPER_ACTOR_INCLUDE_EPAPER_ACTOR_HPP_
 #define UACTOR_ESP32_COMPONENTS_EPAPER_ACTOR_INCLUDE_EPAPER_ACTOR_HPP_
 
+#include <Fonts/FreeMono12pt7b.h>
+#include <gdeh0213b73.h>
+
+#include <map>
+#include <optional>
+#include <string>
 #include <string_view>
 #include <unordered_set>
-#include <map>
 #include <utility>
-#include <optional>
-
-#include <gdeh0213b73.h>
-#include <Fonts/FreeMono12pt7b.h>
 
 #include "actor_runtime/native_actor.hpp"
-#include "pubsub/publication.hpp"
 #include "board_functions.hpp"
+#include "pubsub/publication.hpp"
 
 namespace uActor::ESP32::Notifications {
-  class EPaperNotificationActor : public ActorRuntime::NativeActor {
-    public:
+class EPaperNotificationActor : public ActorRuntime::NativeActor {
+ public:
+  struct State {
+    State(std::string notification_id, std::string node_id,
+          std::string notification_text, size_t number_of_notifications)
+        : notification_id(std::move(notification_id)),
+          node_id(std::move(node_id)),
+          notification_text(std::move(notification_text)),
+          number_of_notifications(number_of_notifications) {}
 
-    struct State {
+    State() = default;
 
-      State(
-        std::string notification_id,
-        std::string node_id,
-        std::string notification_text,
-        size_t number_of_notifications) :
-        notification_id(std::move(notification_id)),
-        node_id(std::move(node_id)),
-        notification_text(std::move(notification_text)),
-        number_of_notifications(number_of_notifications) {};
+    std::string notification_id;
+    std::string node_id;
+    std::string notification_text;
+    size_t number_of_notifications = -1;
 
-      State() = default;
-
-      std::string notification_id;
-      std::string node_id;
-      std::string notification_text;
-      size_t number_of_notifications = -1;
-
-      bool operator==(const State& other) const {
-        return notification_id == notification_id &&
-        node_id == other.node_id &&
-        number_of_notifications == other.number_of_notifications &&
-        notification_text == other.notification_text;
-      }
-    };
-
-    EPaperNotificationActor(ActorRuntime::ManagedNativeActor* actor_wrapper,
-        std::string_view node_id, std::string_view actor_type,
-        std::string_view instance_id);
-
-    void receive (const PubSub::Publication& publication) override;
-
-    void receive_notification(const PubSub::Publication& publication);
-
-    void receive_notification_cancelation(const PubSub::Publication& publication);
-
-    void init();
-
-    void exit();
-
-    void print_next(std::optional<State> state_input = std::nullopt);
-    
-    void cleanup(std::optional<State> state_input = std::nullopt);
-
-    void send_cleanup_trigger();
-
-    void update(State&& state, bool force = false);
-    
-    private:
-    
-    struct Notification {      
-      Notification(std::string id, std::string text, uint32_t lifetime) : id(id), text(std::move(text)), lifetime(lifetime) {}      
-      std::string id;
-      std::string text;
-      uint32_t lifetime;
-    };
-
-    std::map<std::string, Notification> notifications;
-    State current_state;
+    bool operator==(const State& other) const {
+      return notification_id == notification_id && node_id == other.node_id &&
+             number_of_notifications == other.number_of_notifications &&
+             notification_text == other.notification_text;
+    }
   };
 
-} // namespace uActor::ESP32::Notifications
+  EPaperNotificationActor(ActorRuntime::ManagedNativeActor* actor_wrapper,
+                          std::string_view node_id, std::string_view actor_type,
+                          std::string_view instance_id);
 
-#endif // UACTOR_ESP32_COMPONENTS_EPAPER_ACTOR_INCLUDE_EPAPER_ACTOR_HPP_
+  void receive(const PubSub::Publication& publication) override;
+
+  void receive_notification(const PubSub::Publication& publication);
+
+  void receive_notification_cancelation(const PubSub::Publication& publication);
+
+  void init();
+
+  void exit();
+
+  void print_next(std::optional<State> state_input = std::nullopt);
+
+  void cleanup(std::optional<State> state_input = std::nullopt);
+
+  void send_cleanup_trigger();
+
+  void update(State&& state, bool force = false);
+
+ private:
+  struct Notification {
+    Notification(std::string id, std::string text, uint32_t lifetime)
+        : id(id), text(std::move(text)), lifetime(lifetime) {}
+    std::string id;
+    std::string text;
+    uint32_t lifetime;
+  };
+
+  std::map<std::string, Notification> notifications;
+  State current_state;
+};
+
+}  // namespace uActor::ESP32::Notifications
+
+#endif  // UACTOR_ESP32_COMPONENTS_EPAPER_ACTOR_INCLUDE_EPAPER_ACTOR_HPP_
--- uActor-ESP32/components/epaper_actor/src/epaper_actor.cpp
+++ uActor-ESP32/components/epaper_actor/src/epaper_actor.cpp
@@ -1,14 +1,14 @@
 #include "epaper_actor.hpp"
 
-
 EpdSpi io;
 Gdeh0213b73 display(io);
 
 namespace uActor::ESP32::Notifications {
 
-  EPaperNotificationActor::EPaperNotificationActor(ActorRuntime::ManagedNativeActor* actor_wrapper,
-      std::string_view node_id, std::string_view actor_type,
-      std::string_view instance_id) : NativeActor(actor_wrapper, node_id, actor_type, instance_id) {
+EPaperNotificationActor::EPaperNotificationActor(
+    ActorRuntime::ManagedNativeActor* actor_wrapper, std::string_view node_id,
+    std::string_view actor_type, std::string_view instance_id)
+    : NativeActor(actor_wrapper, node_id, actor_type, instance_id) {
   display.init(false);
   display.setRotation(1);
   display.fillScreen(EPD_WHITE);
@@ -16,43 +16,44 @@ namespace uActor::ESP32::Notifications {
 }
 
 void EPaperNotificationActor::receive(const PubSub::Publication& publication) {
-    if(publication.get_str_attr("type") == "notification") {
-      receive_notification(publication);
-    } else if(publication.get_str_attr("type") == "notification_cancelation") {
-      receive_notification_cancelation(publication);
-    } else if(publication.get_str_attr("type") == "init") {
-      init();
-    } else if(publication.get_str_attr("command") == "cleanup") {
-      cleanup();
-      send_cleanup_trigger();
-    } else if(publication.get_str_attr("command") == "scroll_notifications")  {
-      print_next();
-    } else if(publication.get_str_attr("type") == "exit") {
-      exit();
-    }
+  if (publication.get_str_attr("type") == "notification") {
+    receive_notification(publication);
+  } else if (publication.get_str_attr("type") == "notification_cancelation") {
+    receive_notification_cancelation(publication);
+  } else if (publication.get_str_attr("type") == "init") {
+    init();
+  } else if (publication.get_str_attr("command") == "cleanup") {
+    cleanup();
+    send_cleanup_trigger();
+  } else if (publication.get_str_attr("command") == "scroll_notifications") {
+    print_next();
+  } else if (publication.get_str_attr("type") == "exit") {
+    exit();
+  }
 }
 
-void EPaperNotificationActor::receive_notification(const PubSub::Publication& publication) {
-  if(
-    publication.has_attr("notification_id") &&
-    publication.has_attr("notification_lifetime") &&
-    publication.has_attr("notification_text") 
-  ) {
+void EPaperNotificationActor::receive_notification(
+    const PubSub::Publication& publication) {
+  if (publication.has_attr("notification_id") &&
+      publication.has_attr("notification_lifetime") &&
+      publication.has_attr("notification_text")) {
     auto id = std::string(*publication.get_str_attr("notification_id"));
     auto text = std::string(*publication.get_str_attr("notification_text"));
-    auto lifetime = static_cast<uint32_t>(*publication.get_int_attr("notification_lifetime"));
+    auto lifetime = static_cast<uint32_t>(
+        *publication.get_int_attr("notification_lifetime"));
 
-    if(lifetime > 0) {
+    if (lifetime > 0) {
       lifetime += now();
     }
 
-    auto [it, inserted] = notifications.try_emplace(id, Notification(id, text, lifetime));
-    if(!inserted) {
+    auto [it, inserted] =
+        notifications.try_emplace(id, Notification(id, text, lifetime));
+    if (!inserted) {
       it->second.lifetime = std::max(lifetime, it->second.lifetime);
       it->second.text = text;
     }
 
-    if(inserted) {
+    if (inserted) {
       auto new_state = current_state;
       new_state.number_of_notifications = notifications.size();
       new_state.notification_text = text;
@@ -67,9 +68,13 @@ void EPaperNotificationActor::receive_notification(const PubSub::Publication& pu
 }
 
 void EPaperNotificationActor::init() {
-  subscribe(PubSub::Filter{PubSub::Constraint("command", "scroll_notifications"), PubSub::Constraint("node_id", this->node_id().data())});
+  subscribe(
+      PubSub::Filter{PubSub::Constraint("command", "scroll_notifications"),
+                     PubSub::Constraint("node_id", this->node_id().data())});
   subscribe(PubSub::Filter{PubSub::Constraint("type", "notification")});
-  subscribe(PubSub::Filter{PubSub::Constraint("type", "notification_cancelation"), PubSub::Constraint("node_id", this->node_id().data())});
+  subscribe(
+      PubSub::Filter{PubSub::Constraint("type", "notification_cancelation"),
+                     PubSub::Constraint("node_id", this->node_id().data())});
 
   PubSub::Publication register_actor_type;
   register_actor_type.set_attr("type", "unmanaged_actor_update");
@@ -79,26 +84,29 @@ void EPaperNotificationActor::init() {
   publish(std::move(register_actor_type));
 
   send_cleanup_trigger();
-  update(State("", std::string(node_id()), "uActor", notifications.size()), true);
+  update(State("", std::string(node_id()), "uActor", notifications.size()),
+         true);
 }
 
 void EPaperNotificationActor::exit() {
   PubSub::Publication deregister_actor_type;
   deregister_actor_type.set_attr("type", "unmanaged_actor_update");
   deregister_actor_type.set_attr("command", "deregister");
-  deregister_actor_type.set_attr("update_actor_type", "core.notification_center");
+  deregister_actor_type.set_attr("update_actor_type",
+                                 "core.notification_center");
   deregister_actor_type.set_attr("node_id", node_id().data());
   publish(std::move(deregister_actor_type));
 }
 
-void EPaperNotificationActor::receive_notification_cancelation(const PubSub::Publication& publication) {
-  if(publication.has_attr("notification_id")) {
+void EPaperNotificationActor::receive_notification_cancelation(
+    const PubSub::Publication& publication) {
+  if (publication.has_attr("notification_id")) {
     auto id = *publication.get_str_attr("notification_id");
     size_t removed = notifications.erase(std::string(id));
-    if(removed > 0) {
+    if (removed > 0) {
       auto new_state = current_state;
       new_state.number_of_notifications = notifications.size();
-      if(id == current_state.notification_id) {
+      if (id == current_state.notification_id) {
         print_next(std::move(new_state));
       } else {
         update(std::move(new_state));
@@ -108,14 +116,15 @@ void EPaperNotificationActor::receive_notification_cancelation(const PubSub::Pub
 }
 
 void EPaperNotificationActor::print_next(std::optional<State> new_state_input) {
-  auto new_state = new_state_input ? std::move(*new_state_input) : current_state;
+  auto new_state =
+      new_state_input ? std::move(*new_state_input) : current_state;
   // Wrap around
   auto it = notifications.find(current_state.notification_id);
-  if(it == notifications.end() || ++it == notifications.end()) {
+  if (it == notifications.end() || ++it == notifications.end()) {
     it = notifications.begin();
   }
-  
-  if(it != notifications.end()) {
+
+  if (it != notifications.end()) {
     new_state.notification_id = it->second.id;
     new_state.notification_text = it->second.text;
   } else {
@@ -126,22 +135,22 @@ void EPaperNotificationActor::print_next(std::optional<State> new_state_input) {
 }
 
 void EPaperNotificationActor::cleanup(std::optional<State> new_state_input) {
-
-  auto new_state = new_state_input ? std::move(*new_state_input) : current_state;
+  auto new_state =
+      new_state_input ? std::move(*new_state_input) : current_state;
 
   std::unordered_set<std::string> to_delete;
-  for(const auto& [id, notification] : notifications) {
-    if(notification.lifetime > 0 && notification.lifetime < now()) {
+  for (const auto& [id, notification] : notifications) {
+    if (notification.lifetime > 0 && notification.lifetime < now()) {
       to_delete.insert(id);
     }
   }
-  for(const auto& id : to_delete) {
+  for (const auto& id : to_delete) {
     notifications.erase(id);
   }
 
-  if(to_delete.size() > 0) {
+  if (to_delete.size() > 0) {
     new_state.number_of_notifications = notifications.size();
-    if(to_delete.find(current_state.notification_id) != to_delete.end()) {
+    if (to_delete.find(current_state.notification_id) != to_delete.end()) {
       print_next(std::move(new_state));
     } else {
       update(std::move(new_state));
@@ -159,26 +168,26 @@ void EPaperNotificationActor::send_cleanup_trigger() {
 }
 
 void EPaperNotificationActor::update(State&& new_state, bool force) {
-
-  if(new_state == current_state && !force) {
+  if (new_state == current_state && !force) {
     return;
   } else {
     current_state = std::move(new_state);
   }
-  
+
   display.fillScreen(EPD_WHITE);
-  display.setCursor(0,14);
+  display.setCursor(0, 14);
   // This is seems to be a bug in the underlying library
   display.setTextColor(EPD_WHITE);
 
   display.setFont(&FreeMono12pt7b);
-  display.println(current_state.node_id + " - " + std::to_string(current_state.number_of_notifications));
+  display.println(current_state.node_id + " - " +
+                  std::to_string(current_state.number_of_notifications));
 
-  for(int i = 0; i < 250; i++) {
-      display.drawPixel(i, 22, 1); 
+  for (int i = 0; i < 250; i++) {
+    display.drawPixel(i, 22, 1);
   }
 
   display.println(current_state.notification_text);
   display.update();
 }
-}
+}  // namespace uActor::ESP32::Notifications
