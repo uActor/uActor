aef01caba8da98a5ed263c19b3847c3812b86cbb
Kili <kili+git@mailbox.org>
2021-03-10 11:23:23 +0100

    Switch to Debian for ARM CI

--- toolchains/armv7_linux.cmake
+++ toolchains/armv7_linux.cmake
@@ -4,11 +4,7 @@ set(CONAN_ARCHITECTURE armv7)
 
 set(CMAKE_SYSTEM_ROOT /usr/arm-linux-gnueabihf)
 
-set(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc)
-set(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++)
-
-#set(COMPILE_FLAGS "-mcpu=cortex-a9 -mfpu=neon -ftree-vectorize")
-#set(CMAKE_C_FLAGS ${COMPILE_FLAGS})
-#set(CMAKE_CXX_FLAGS ${COMPILE_FLAGS})
+set(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc-8)
+set(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++-8)
 
 set(CMAKE_EXE_LINKER_FLAGS "-static-libgcc -static-libstdc++")
