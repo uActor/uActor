5260ca3df45c085e0e9a259f4fb21ab77d0a9d2b
Raphael Hetzel <hetzel@in.tum.de>
2021-05-07 17:09:14 +0200

    Move legacy examples

---
+++ examples/docsem2020/simplified.lua
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+function receive(message)
+  if(message.type == "init") then
+    subscribe({type="sensor_update_co2",
+               publisher_node_id=node_id})
+  elseif(message.type == "sensor_update_co2") then
+    local quality_level = "BAD"
+    if(message.value < 1000) then
+      quality_level = "GOOD"
+    end
+    publish(Publication.new(
+      "display_text", "Air Quality:\n"..quality_level
+    ))
+  end
+end
