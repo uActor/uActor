474d0bb00bbcdbe4f838b10dbb5707964acd55e9
Raphael Hetzel <hetzel@in.tum.de>
2021-05-21 17:33:22 +0200

    Remove old example code.

--- examples/legacy_examples/pong.lua
+++
@@ -1,9 +0,0 @@
-function receive(message)
-  if(message.type == "init") then
-    subscribe({message="ping", publisher_node_id=node_id})
-  end
-  if(message.message == "ping") then
-    print("ping")
-    publish({node_id=message.publisher_node_id, actor_type=message.publisher_actor_type, instance_id=message.publisher_instance_id, message="pong"})
-  end
-end
