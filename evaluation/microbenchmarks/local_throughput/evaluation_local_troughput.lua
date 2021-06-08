78756fc992edde4afacedce09b8cf21fac47a66d
Raphael Hetzel <hetzel@in.tum.de>
2021-06-09 00:10:08 +0200

    Revert to the old (smaller, balanced) throughput test

--- evaluation/microbenchmarks/local_throughput/evaluation_local_troughput.lua
+++ evaluation/microbenchmarks/local_throughput/evaluation_local_troughput.lua
@@ -1,18 +1,15 @@
 function receive(message)
   if(message.exp_1 == "ping") then
     count = count + 1
-    if(count < 10000) then
-      publish(Publication.new("exp_1", "ping"))
-    else
+    if(count == 10000) then
       testbed_stop_timekeeping(1, "time_for_10000")
       count = 0
-      enqueue_wakeup(1000, "start_run")
+      testbed_start_timekeeping(1)
     end
+    publish(Publication.new("exp_1", "ping"))
   elseif(message.type == "init") then
     subscribe({exp_1="ping"})
     count = 0
-    enqueue_wakeup(10000, "start_run")
-  elseif(message.type == "wakeup" and message.wakeup_id == "start_run") then
     collectgarbage()
     testbed_start_timekeeping(1)
     publish(Publication.new("exp_1", "ping"))
