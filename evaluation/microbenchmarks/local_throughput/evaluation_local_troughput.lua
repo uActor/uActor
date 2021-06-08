14321f80dadcf575e8b1a1aa01acf50f5436a47e
Raphael Hetzel <hetzel@in.tum.de>
2021-06-08 15:08:03 +0200

    Fix & Improve throughput test

--- evaluation/microbenchmarks/local_throughput/evaluation_local_troughput.lua
+++ evaluation/microbenchmarks/local_throughput/evaluation_local_troughput.lua
@@ -1,15 +1,18 @@
 function receive(message)
   if(message.exp_1 == "ping") then
     count = count + 1
-    if(count == 10000) then
+    if(count < 10000) then
+      publish(Publication.new("exp_1", "ping"))
+    else
       testbed_stop_timekeeping(1, "time_for_10000")
-      testbed_start_timekeeping(1)
       count = 0
+      enqueue_wakeup(1000, "start_run")
     end
-    publish(Publication.new("exp_1", "ping"))
   elseif(message.type == "init") then
     subscribe({exp_1="ping"})
     count = 0
+    enqueue_wakeup(10000, "start_run")
+  elseif(message.type == "wakeup" and message.wakeup_id == "start_run") then
     collectgarbage()
     testbed_start_timekeeping(1)
     publish(Publication.new("exp_1", "ping"))
