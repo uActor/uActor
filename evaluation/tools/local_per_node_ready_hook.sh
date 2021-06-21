8f7be67c2aef1591170c5a8da23638d9e6792016
Raphael Hetzel <hetzel@in.tum.de>
2021-06-21 12:49:44 +0200

    Fix evaluation script

--- evaluation/tools/local_per_node_ready_hook.sh
+++ evaluation/tools/local_per_node_ready_hook.sh
@@ -1,6 +1,6 @@
 sleep 30
 
-for ip in $(python3 $TESTBED_ROOT_DIR/../tools/testbed_ips.py)
+for ip in $(python3 $TESTBED_ROOT_DIR/../evaluation/tools/testbed_ips.py)
 do
   python3 $TESTBED_ROOT_DIR/../tools/actorctl.py -f $TESTBED_CONFIG_DIR/$1 ${ip} 1337
 done
