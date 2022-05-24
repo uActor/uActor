8cd6c6ab461b5cf25f11247bae1c181942f86a75
Kili <kili+git@mailbox.org>
2022-05-24 12:14:32 +0200

    Fix the crossbuild toolchain files

--- toolchains/arm64_linux.cmake
+++ toolchains/arm64_linux.cmake
@@ -3,7 +3,7 @@ set(CMAKE_SYSTEM_PROCESSOR armv8)
 
 set(CMAKE_SYSTEM_ROOT /usr/aarch64-linux-gnu)
 
-set(CMAKE_C_COMPILER aarch64-linux-gnu-gcc-8)
-set(CMAKE_CXX_COMPILER aarch64-linux-gnu-g++-8)
+set(CMAKE_C_COMPILER aarch64-linux-gnu-gcc-10)
+set(CMAKE_CXX_COMPILER aarch64-linux-gnu-g++-10)
 
 set(CMAKE_EXE_LINKER_FLAGS "-static-libgcc -static-libstdc++")
--- toolchains/armv7_linux.cmake
+++ toolchains/armv7_linux.cmake
@@ -4,7 +4,7 @@ set(CONAN_ARCHITECTURE armv7)
 
 set(CMAKE_SYSTEM_ROOT /usr/arm-linux-gnueabihf)
 
-set(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc-8)
-set(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++-8)
+set(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc-10)
+set(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++-10)
 
 set(CMAKE_EXE_LINKER_FLAGS "-static-libgcc -static-libstdc++")
